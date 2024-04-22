0278644c93096a08c770ee51dad26dc1a98f7444
Nick Patrick <nckptrck1@gmail.com>
2024-04-22 10:05:21 -0700

    grid search

--- scripts/sng/depth-weight-exploration.R
+++ scripts/sng/depth-weight-exploration.R
@@ -50,8 +50,8 @@ depth_weight_fn <- function(depth, alpha, beta){
 alpha.vec <- seq(2,11,1)
 beta.vec <- seq(1,15,1)
 
-avgDivsDF <- data.frame(0,0,0)
-names(avgDivsDF) = c("alpha", "beta", "avgDiv")
+gs1 <- data.frame(0,0,0,0,0,0,0,0,0)
+names(gammaDF) = c("alpha", "beta", "avgDiv", "SD", "Min", "25", "50","75", "Max")
 
 
 for (a in alpha.vec){
@@ -83,23 +83,211 @@ alpha_divs <- sampinfo %>%
 
 alpha_div <- alpha_divs |> 
   slice(-14) |> 
-  summarise(avgDiv = mean(alpha.div.sh))
+  summarise(avgDiv = mean(alpha.div.sh),
+            sd = sd(alpha.div.sh),
+            min = min(alpha.div.sh),,
+            twentyfifth = quantile(alpha.div.sh, .25),
+            fifty = quantile(alpha.div.sh, .50),
+            seventyfifth = quantile(alpha.div.sh, .75),
+            max = max(alpha.div.sh))
 
-newRow = c(a,b, alpha_div$avgDiv)
+newRow = c(a,b, alpha_div$avgDiv, alpha_div$sd, alpha_div$min, alpha_div$twentyfifth, alpha_div$fifty, alpha_div$seventyfifth, alpha_div$max)
 
 print(newRow)
 
-avgDivsDF <- rbind(avgDivsDF, setNames(newRow,names(avgDivsDF)))
+gs1 <- rbind(gs1, setNames(newRow,names(gs1)))
   }
 }
 
 
-avgDivsDF <- avgDivsDF |> 
+gs1 <- gs1 |> 
   slice(-1)
 
-avgDivsDF |> 
-  filter((alpha-1)* beta >= 10)
+
+save(gs1, file = "rslt/grid-search-result-wide.RData")
+
+
+gs1 |> 
+  filter(avgDiv == max(gs1$avgDiv))
 
 library(rgl)
 
-with(avgDivsDF, plot3d(x = alpha, y = beta, z = avgDiv))
+with(gs1, plot3d(x = alpha, y = beta, z = SD))
+
+##########################################
+## Other Distributions ###################
+##########################################
+
+# Normal
+# Target certain depth ranges??
+
+
+mu.vec = c(10,25,50,75,100)
+sigma.vec = c(5,10,15)
+
+depth_weight_fn2 <- function(depth, mu, sigma){
+  dnorm(depth, mean = mu, sd= sigma) ##CHANGE????
+}
+
+normalDF <- data.frame(0,0,0,0,0,0,0,0,0)
+names(normalDF) = c("mu", "sigma", "avgDiv", "SD", "Min", "25", "50","75", "Max")
+
+for (a in mu.vec){
+  for (b in sigma.vec){
+    edna_agg <- edna_imputed |>
+      mutate(depthm = as.numeric(depthm),
+             weight = depth_weight_fn2(depthm, a, b)) |>
+      group_by(cruise, line, sta) |>
+      summarize(across(starts_with('asv'), 
+                       ~weighted.mean(log(.x), weight)), ## automatically normalizes weights
+                .groups = 'drop') |>
+      group_by(cruise, line) |>
+      summarize(across(starts_with('asv'), ~mean(.x))) |>
+      group_by(cruise) |>
+      summarize(across(starts_with('asv'), ~mean(.x)))
+    
+    edna_data <- edna_agg |> mutate(exp(pick(-cruise)))
+    
+    # split into sample info and asvs
+    asv <- edna_data |> 
+      dplyr::select(starts_with('asv'))
+    
+    sampinfo <- edna_data |> 
+      dplyr::select(-starts_with('asv'))
+    
+    # alpha diversity measures
+    alpha_divs <- sampinfo %>%
+      bind_cols(alpha.div.sh = diversity(asv, index = 'shannon')) 
+    
+    alpha_div <- alpha_divs |> 
+      slice(-14) |> 
+      summarise(avgDiv = mean(alpha.div.sh),
+                sd = sd(alpha.div.sh),
+                min = min(alpha.div.sh),,
+                twentyfifth = quantile(alpha.div.sh, .25),
+                fifty = quantile(alpha.div.sh, .50),
+                seventyfifth = quantile(alpha.div.sh, .75),
+                max = max(alpha.div.sh))
+    
+    newRow = c(a,b, alpha_div$avgDiv, alpha_div$sd, alpha_div$min, alpha_div$twentyfifth, alpha_div$fifty, alpha_div$seventyfifth, alpha_div$max)
+    
+    print(newRow)
+    
+    normalDF <- rbind(normalDF, setNames(newRow,names(normalDF)))
+  }
+}
+
+
+normalDF <- normalDF |> 
+  slice(-1)
+
+
+
+### Summarize sample sizes div by depth
+## zoom in to upper corner alpha = 1 ->5, beta = 1 ->5
+## is optimal better than raw average?
+## different thing to optimize?
+
+# calc raw mean
+edna_agg <- edna_imputed |>
+  mutate(depthm = as.numeric(depthm),
+         weight = 1) |>
+  group_by(cruise, line, sta) |>
+  summarize(across(starts_with('asv'), 
+                   ~weighted.mean(log(.x), weight)), ## automatically normalizes weights
+            .groups = 'drop') |>
+  group_by(cruise, line) |>
+  summarize(across(starts_with('asv'), ~mean(.x))) |>
+  group_by(cruise) |>
+  summarize(across(starts_with('asv'), ~mean(.x)))
+
+edna_data <- edna_agg |> mutate(exp(pick(-cruise)))
+
+# split into sample info and asvs
+asv <- edna_data |> 
+  dplyr::select(starts_with('asv'))
+
+sampinfo <- edna_data |> 
+  dplyr::select(-starts_with('asv'))
+
+# alpha diversity measures
+alpha_divs_raw <- sampinfo %>%
+  bind_cols(alpha.div.sh = diversity(asv, index = 'shannon'))
+
+## Raw mean
+alpha_divs_raw |> 
+  summarise(meanDiv = mean(alpha.div.sh))
+
+
+######################################################
+# More Precise Grid Search alpha(2:5) beta(1:5) by 0.1
+######################################################
+
+depth_weight_fn <- function(depth, alpha, beta){
+  dgamma(depth, shape = alpha, scale = beta) ##CHANGE????
+}
+
+alpha.vec <- seq(2,5,0.1)
+beta.vec <- seq(1,5,0.1)
+
+gs2 <- data.frame(0,0,0,0,0,0,0,0,0)
+names(gs2) = c("alpha", "beta", "avgDiv", "SD", "Min", "25", "50","75", "Max")
+
+
+for (a in alpha.vec){
+  for (b in beta.vec){
+    edna_agg <- edna_imputed |>
+      mutate(depthm = as.numeric(depthm),
+             weight = depth_weight_fn(depthm, a, b)) |>
+      group_by(cruise, line, sta) |>
+      summarize(across(starts_with('asv'), 
+                       ~weighted.mean(log(.x), weight)), ## automatically normalizes weights
+                .groups = 'drop') |>
+      group_by(cruise, line) |>
+      summarize(across(starts_with('asv'), ~mean(.x))) |>
+      group_by(cruise) |>
+      summarize(across(starts_with('asv'), ~mean(.x)))
+    
+    edna_data <- edna_agg |> mutate(exp(pick(-cruise)))
+    
+    # split into sample info and asvs
+    asv <- edna_data |> 
+      dplyr::select(starts_with('asv'))
+    
+    sampinfo <- edna_data |> 
+      dplyr::select(-starts_with('asv'))
+    
+    # alpha diversity measures
+    alpha_divs <- sampinfo %>%
+      bind_cols(alpha.div.sh = diversity(asv, index = 'shannon')) 
+    
+    alpha_div <- alpha_divs |> 
+      slice(-14) |> 
+      summarise(avgDiv = mean(alpha.div.sh),
+                sd = sd(alpha.div.sh),
+                min = min(alpha.div.sh),,
+                twentyfifth = quantile(alpha.div.sh, .25),
+                fifty = quantile(alpha.div.sh, .50),
+                seventyfifth = quantile(alpha.div.sh, .75),
+                max = max(alpha.div.sh))
+    
+    newRow = c(a,b, alpha_div$avgDiv, alpha_div$sd, alpha_div$min, alpha_div$twentyfifth, alpha_div$fifty, alpha_div$seventyfifth, alpha_div$max)
+    
+    print(newRow)
+    
+    gs2 <- rbind(gs2, setNames(newRow,names(gs2)))
+  }
+}
+
+
+gs2 <- gs2 |> 
+  slice(-1)
+
+save(gs2, file = "rslt/grid-search-narrow.RData")
+
+gs2 |> 
+  filter(avgDiv == max(gammaDF$avgDiv))
+
+library(rgl)
+
+with(gs2, plot3d(x = alpha, y = beta, z = avgDiv))
