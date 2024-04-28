2782a558462f6d66dad456379888822c75076ca7
ruizt <75809313+ruizt@users.noreply.github.com>
2024-04-27 19:42:55 -0700

    clean up scripts

--- scripts/model-fitting.R
+++ scripts/model-fitting.R
@@ -1,79 +1,12 @@
-library(compositions)
-library(pls)
-library(spls)
 library(tidyverse)
-library(RColorBrewer)
+library(spls)
+library(openxlsx)
 
+load('data/ncog-18s-processed-2024-04-27.RData')
+load('data/ceta-density-processed-2024-04-27.RData')
 
-## DATA PREPARATION ------------------------------------------------------------
-
-# load aggregated 18s relative abundances
-load("data/edna_18s_processed.RData")
-head(edna_data)
-
-# read in taxonomic classifications
-taxa <- read_tsv('data/NCOG_18sV9_asv_count_tax_S.tsv') |>
-  mutate(short.id = paste('asv', row_number(), sep = '.')) |> 
-  dplyr::select(where(is.character), silva_Confidence)
-
-# import whale sighting data
-scaled_sightings <- read_csv("data/CC_on_effort_scaled_sightings_Ceta_SCB.csv") |>
-  rename_with(tolower) |>
-  rename_with(~gsub('_.*', '', .x)) |>
-  mutate(cruise = str_replace(cruiseid, "CC", "X20")) |>
-  dplyr::select(cruise, season, bp, bm, mn)
-
-# impute zeroes with uniform random numbers up to seasonal minima
-set.seed(30924)
-scaled_sightings_imp <- scaled_sightings |>
-  group_by(season) |>
-  summarize(across(where(is.numeric), 
-                   .fns = list(min = ~min(.x[.x > 0], na.rm = T)),
-                   .names = '{.col}.{.fn}')) |>
-  right_join(scaled_sightings, by = 'season') |>
-  mutate(bp.imp = if_else(bp == 0, 
-                          runif(n = length(bp == 0), min = 0, max = bp.min),
-                          bp),
-         bm.imp = if_else(bm == 0,
-                          runif(n = length(bm == 0), min = 0, max = bm.min),
-                          bm),
-         mn.imp = if_else(mn == 0,
-                          runif(n = length(mn == 0), min = 0, max = mn.min),
-                          mn))
-
-# subtract seasonal averages
-sightings <- scaled_sightings_imp |>
-  dplyr::select(cruise, season, ends_with('imp')) |>
-  group_by(season) |>
-  summarize(across(ends_with('imp'), 
-                   .fns = list(mean = ~mean(log(.x), na.rm = T)),
-                   .names = 'log.{.col}.{.fn}')) |>
-  right_join(scaled_sightings_imp) |>
-  mutate(cruise = cruise,
-            bp = log(bp.imp) - log.bp.imp.mean,
-            bm = log(bm.imp) - log.bm.imp.mean,
-            mn = log(mn.imp) - log.mn.imp.mean,
-         .keep = "none")
-
-# clr transformation
-clr_out <- edna_data |> 
-  dplyr::select(-cruise) |> 
-  clr() |>
-  as_tibble()
-
-# center wrt seasonal (geometric) mean
-clr_centered <- edna_data |>
-  dplyr::select(cruise) |>
-  bind_cols(clr_out) |>
-  left_join(dplyr::select(scaled_sightings, cruise, season),
-            by = 'cruise') |>
-  group_by(season) |>
-  mutate(across(starts_with('asv'), ~.x - mean(.x))) |>
-  ungroup() |>
-  dplyr::select(cruise, starts_with('asv'))
-
-# combine sightings and edna
-whales <- inner_join(sightings, clr_centered, by = 'cruise')
+# combine seasonally adjusted density estimates and seasonally adjusted edna data
+whales <- inner_join(log_density_estimates_adj, edna_clr_adj, by = 'cruise')
 
 ## MODEL FITTING ---------------------------------------------------------------
 
@@ -124,10 +57,10 @@ eta_df |>
   scale_y_log10() + 
   geom_vline(aes(xintercept = eta.approx), 
              data = tibble(species = c('bm', 'bp', 'mn'),
-                           eta.approx = c(0.48, 0.61, 0.45)))
+                           eta.approx = c(0.65, 0.58, 0.635)))
 
 eta_sel <- tibble(species = c('bm', 'bp', 'mn'),
-                  eta.approx = c(0.48, 0.61, 0.45))
+                  eta.approx = c(0.65, 0.58, 0.635))
 
 # choose closest grid points to selected eta
 mspe_df <- eta_df |>
@@ -149,7 +82,7 @@ fit_bp <- spls(x, bp, K = 2, eta = eta_bp,
 fit_mn <- spls(x, mn, K = 2, eta = eta_mn, 
                scale.x = F, scale.y = F)
 
-## MODEL OUTPUTS
+## MODEL OUTPUTS ---------------------------------------------------------------
 
 # extract predictions
 pred_bp <- predict(fit_bp)
@@ -161,8 +94,7 @@ whale_summaries <- whales |>
   dplyr::select(2:4) |>
   pivot_longer(everything(), names_to = 'species', values_to = 'density') |>
   group_by(species) |>
-  summarize(mean.log.density = mean(density),
-            var.log.density = var(density)) 
+  summarize(var.log.adj.density = var(density)) 
 
 fit_summary <- tibble(species = c('bp', 'bm', 'mn'),
                       n.asv = c(nrow(fit_bp$projection),
@@ -176,17 +108,6 @@ fit_summary <- tibble(species = c('bp', 'bm', 'mn'),
   dplyr::select(-eta)
 
 fit_summary
-
-
-# # verify fitted model components
-# x_sel <- dplyr::select(x, rownames(fit$projection))
-# x_sel_proj <- as.matrix(x_sel) %*% fit$projection
-# 
-# lm(y ~ x_sel_proj)$fitted |> 
-#   bind_cols(fitted_vals)
-# 
-# (fit$projection %*% lm(y ~ x_sel_proj)$coef[2:3]) |>
-#   bind_cols(fit$betahat[fit$betahat != 0])
 
 # extract selected asvs
 asv_bp <- tibble(short.id = colnames(x)[fit_bp$A],
@@ -207,7 +128,6 @@ asv_mn <- tibble(short.id = colnames(x)[fit_mn$A],
   separate(silva_Taxon, into = c('d', 'p', 'c', 'o', 'f', 'g'), sep = ';') |>
   arrange(coef)
 
-
 # join asvs across species
 asv_sel <- full_join(asv_bm, asv_bp, 
           by = c('short.id', 'd', 'p', 'c', 'o', 'f', 'g'),
@@ -215,6 +135,34 @@ asv_sel <- full_join(asv_bm, asv_bp,
   full_join(asv_mn, 
             by = c('short.id', 'd', 'p', 'c', 'o', 'f', 'g')) |>
   rename(coef.mn = coef) 
+
+# export selected asvs and fit summaries as excel workbook
+xl_out <- list(blue = mutate(asv_bm, coef = 100*(2^coef - 1)),
+               fin = mutate(asv_bp, coef = 100*(2^coef - 1)),
+               humpback = mutate(asv_mn, coef = 100*(2^coef - 1)),
+               fit.summary = fit_summary)
+
+openxlsx::write.xlsx(xl_out, 
+           paste('rslt/tbl/fit-tables-', today(), '.xlsx', sep = ''), 
+           rownames = F)
+
+
+
+## DRAFT MATERIAL --------------------------------------------------------------
+
+count(group_by(asv_bm, p), name = 'bm') |>
+  full_join(count(group_by(asv_bp, p), name = 'bp'), by = 'p') |>
+  full_join(count(group_by(asv_mn, p), name = 'mn'), by = 'p')
+
+# # verify fitted model components
+# x_sel <- dplyr::select(x, rownames(fit$projection))
+# x_sel_proj <- as.matrix(x_sel) %*% fit$projection
+# 
+# lm(y ~ x_sel_proj)$fitted |> 
+#   bind_cols(fitted_vals)
+# 
+# (fit$projection %*% lm(y ~ x_sel_proj)$coef[2:3]) |>
+#   bind_cols(fit$betahat[fit$betahat != 0])
 
 # visualize
 pal <- colorRampPalette(c('#649dfa', '#02204f'), bias = 1)
@@ -324,22 +272,3 @@ asv_bp |>
 
 ggsave(filename = 'rslt/plots/42524/asv-sel-bp.png',
        height = 4, width = 5, dpi = 300)
-
-
-# tables
-library(openxlsx)
-
-xl_out <- list(bm = mutate(asv_bm, coef = 100*(2^coef - 1)),
-     bp = mutate(asv_bp, coef = 100*(2^coef - 1)),
-     mn = mutate(asv_mn, coef = 100*(2^coef - 1)),
-     fit_summary = fit_summary)
-
-write.xlsx(xl_out, 'rslt/plots/42524/tables.xlsx', rownames = F)
-
-count(group_by(asv_bm, p), name = 'bm') |>
-  full_join(count(group_by(asv_bp, p), name = 'bp'), by = 'p') |>
-  full_join(count(group_by(asv_mn, p), name = 'mn'), by = 'p')
-
-
-fit_summary
-          
---
+++ scripts/preprocessing.R
@@ -0,0 +1,196 @@
+library(tidyverse)
+library(compositions)
+library(zCompositions)
+
+## 18SV9 EDNA DATA -------------------------------------------------------------
+
+## IMPORT READ COUNTS
+
+# read in metadata
+metadata <- read_csv("data/raw/NCOG_sample_log_DNA_meta_2014-2020.csv")
+
+# read in 18s reads
+edna_in <- read_tsv('data/raw/NCOG_18sV9_asv_count_tax_S.tsv') |>
+  mutate(short.id = paste('asv', row_number(), sep = '.'))
+
+# retain taxon names
+taxa <- edna_in |> 
+  dplyr::select(where(is.character), silva_Confidence, pr2_Confidence)
+
+# transpose and clean up names
+edna_raw <- edna_in |>
+  dplyr::select(-starts_with('silva'), -starts_with('pr2'), -Feature.ID) |>
+  pivot_longer(-short.id, names_to = 'sample.id', values_to = 'read.count') |>
+  pivot_wider(names_from = short.id, values_from = read.count) |> 
+  separate(sample.id, 
+           into = c('cruise', 'line', 'sta', 'depthm'), 
+           sep = '_',
+           remove = F)
+
+# # inspect samples without line or station assignment
+# edna_raw |>
+#   filter(if_any(c(cruise, line, sta, depthm), is.na)) |>
+#   pull(sample.id)
+
+# extract genuine samples
+edna_samples <- edna_raw |>
+  filter(if_all(c(cruise, line, sta, depthm), ~!is.na(.x))) 
+
+## FILTERING AND IMPUTATION
+
+# select asv's that appear in at least 5% of samples and less than 90% of samples
+cols_of_interest <- edna_samples |>
+  dplyr::select(starts_with('asv')) |>
+  as.matrix() |>
+  apply(2, function(.x){mean(.x > 0)}) |>
+  t() |>
+  as_tibble() |>
+  gather(col, prop.nz) |>
+  filter(prop.nz > 0.05, prop.nz < 0.9) |> 
+  pull(col)
+
+# filter samples in which fewer than 10% of asv's of interest are present
+rows_of_interest <- edna_samples |> 
+  dplyr::select(all_of(cols_of_interest)) |>
+  mutate(across(where(is.numeric), ~ .x > 0)) |>
+  mutate(total = rowMeans(pick(starts_with('asv'))),
+         rownum = row_number()) |>
+  dplyr::select(total, rownum) |>
+  filter(total > 0.1) |>
+  pull(rownum)
+
+# # check total number of zero reads (how much of the data will be imputed?)
+# edna_samples |>
+#   dplyr::select(all_of(cols_of_interest)) |>
+#   slice(rows_of_interest) |>
+#   gather() |>
+#   summarize(nvals = n(),
+#             num.nz = sum(value != 0),
+#             prop.nz = mean(value != 0))
+
+# # check asv presence/absence frequencies across samples after filtering
+# edna_samples |> 
+#   dplyr::select(all_of(cols_of_interest)) |>
+#   slice(rows_of_interest) |>
+#   as.matrix() |>
+#   apply(2, function(.x){mean(.x > 0)}) |>
+#   t() |>
+#   as_tibble() |>
+#   gather(col, prop.nz) |>
+#   arrange(prop.nz) |>
+#   summarize(n = sum(prop.nz > 0.01),
+#             prop = n/n())
+
+# impute zeros ( x_{ijkl} )
+imputation_out <- edna_samples |> 
+  dplyr::select(all_of(cols_of_interest)) |>
+  slice(rows_of_interest) |>
+  zCompositions::cmultRepl(label = 0, 
+                           method = 'GBM', 
+                           output = 'prop',
+                           z.warning = 0.99)
+
+# bind imputed values to sample info
+edna_imputed <- edna_samples |> 
+  slice(rows_of_interest) |>
+  dplyr::select(1:5) |>
+  bind_cols(imputation_out)
+
+# save imputed data, for grid search (remove later)
+save(edna_imputed, file = paste('data/_draft/edna-imputed-', today(), '.RData', sep = ''))
+
+## AGGREGATION
+
+# function for weighting by depth
+depth_weight_fn <- function(depth){
+  dgamma(depth, shape = 2, scale = 5)
+}
+
+# average first over depth, then over station, then over transect ( x_{ij} )
+# interpretation: average proportion of asv.XX across cruise is ZZ
+edna_aggregated <- edna_imputed |>
+  mutate(depthm = as.numeric(depthm),
+         weight = depth_weight_fn(depthm)) |>
+  group_by(cruise, line, sta) |>
+  summarize(across(starts_with('asv'), 
+                   ~weighted.mean(log(.x), weight)), 
+            .groups = 'drop') |>
+  group_by(cruise, line) |>
+  summarize(across(starts_with('asv'), ~mean(.x))) |>
+  group_by(cruise) |>
+  summarize(across(starts_with('asv'), ~mean(.x))) |> 
+  mutate(exp(pick(-cruise)))
+
+## SEASONAL DE-TRENDING
+
+# clr transformation ( log[z_{ij}] = log[x_{ij}/g_{i}] )
+edna_clr <- edna_aggregated |> 
+  dplyr::select(-cruise) |> 
+  clr() |>
+  as_tibble()
+
+# center wrt seasonal (geometric) mean ( log[z_{ij}/g_{z_{j}}(i)] )
+edna_clr_adj <- edna_aggregated |>
+  dplyr::select(cruise) |>
+  mutate(qtr = str_remove(cruise, 'X') |> ym() |> quarter()) |>
+  mutate(qtr = if_else(cruise == 'X201806', 3, qtr)) |>
+  bind_cols(edna_clr) |>
+  group_by(qtr) |>
+  mutate(across(starts_with('asv'), ~.x - mean(.x))) |>
+  ungroup() |>
+  dplyr::select(cruise, starts_with('asv'))
+
+# export
+save(list = c('metadata', 
+              'taxa', 
+              'edna_samples', 
+              'edna_imputed', 
+              'edna_aggregated', 
+              'edna_clr_adj'), 
+     file = paste('data/ncog-18s-processed-', today(), '.RData', sep = ''))
+
+## SCALED SIGHTING DATA --------------------------------------------------------
+
+# import whale sighting data ( y_{i} )
+density_estimates <- read_csv("data/raw/CC_on_effort_scaled_sightings_Ceta_SCB.csv") |>
+  rename_with(tolower) |>
+  rename_with(~gsub('_.*', '', .x)) |>
+  mutate(cruise = str_replace(cruiseid, "CC", "X20")) |>
+  dplyr::select(cruise, season, bp, bm, mn)
+
+# impute zeroes with uniform random numbers up to seasonal minima (no notation yet)
+set.seed(30924)
+density_estimates_imputed <- density_estimates |>
+  group_by(season) |>
+  summarize(across(where(is.numeric), 
+                   .fns = list(min = ~min(.x[.x > 0], na.rm = T)),
+                   .names = '{.col}.{.fn}')) |>
+  right_join(density_estimates, by = 'season') |>
+  mutate(bp.imp = if_else(bp == 0, 
+                          runif(n = length(bp == 0), min = 0, max = bp.min),
+                          bp),
+         bm.imp = if_else(bm == 0,
+                          runif(n = length(bm == 0), min = 0, max = bm.min),
+                          bm),
+         mn.imp = if_else(mn == 0,
+                          runif(n = length(mn == 0), min = 0, max = mn.min),
+                          mn))
+
+# seasonal de-trending ( log[y_{i}/g_{y}(i)] )
+log_density_estimates_adj <- density_estimates_imputed |>
+  dplyr::select(cruise, season, ends_with('imp')) |>
+  group_by(season) |>
+  summarize(across(ends_with('imp'), 
+                   .fns = list(mean = ~mean(log(.x), na.rm = T)),
+                   .names = 'log.{.col}.{.fn}')) |>
+  right_join(density_estimates_imputed) |>
+  mutate(cruise = cruise,
+         bp = log(bp.imp) - log.bp.imp.mean,
+         bm = log(bm.imp) - log.bm.imp.mean,
+         mn = log(mn.imp) - log.mn.imp.mean,
+         .keep = "none")
+
+# export
+save(list = c('density_estimates', 'density_estimates_imputed', 'log_density_estimates_adj'), 
+     file = paste('data/ceta-density-processed-', today(), '.RData', sep = ''))
+
