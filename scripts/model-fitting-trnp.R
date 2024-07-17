00071dac2c39d0c191c17e5109f6102258e15827
ruizt <75809313+ruizt@users.noreply.github.com>
2024-07-17 11:53:25 -0700

    update stability selection setup

--- scripts/model-fitting-trnp.R
+++ scripts/model-fitting-trnp.R
@@ -6,3 +6,16 @@ sel_freq <- read_rds('rslt/loocv/2024-07-15/selection-frequencies.rds')
 
 # read in metrics from LOOCV
 metrics <- read_rds('rslt/loocv/2024-07-15/metrics.rds')
+
+## SPECIFYING NCOMP/ETA RANGE --------------------------------------------------
+# chosen stability threshold (minimum max selection prob.)
+pimax <- 0.8
+
+# desired bound for expected number of false positives
+EV <- 5
+
+# total number of candidate ASVs
+p <- 3248
+
+# threshold for max average number of selected ASVs
+qmax <- sqrt((2*pi_max - 1)*p*EV) 
